30985e03da0ba345d307a7709385a323fc867bd3
DESKTOP-B5IRF34\shuoya <timehet556947@gmail.com>
2023-05-01 17:42:35 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", fibonacci(5));
+}
+fn fibonacci(term: i32) -> i32 {
+    if term == 0 {
+        return 0;
+    } else if term == 1 {
+        return 1;
+    } else {
+        return fibonacci(term - 1) + fibonacci(term - 2);
+    }
 }
